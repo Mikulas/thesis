895dd5f29763535c503d17de11398bbb42c4ea22
Mikuláš Dítě <dite@mangoweb.cz>
2018-12-29 16:50:47 +0100

    p2 redis

--- src/projects/p2-dynamic/src/Controller/PostListController.php
+++ src/projects/p2-dynamic/src/Controller/PostListController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -14,10 +16,15 @@ class PostListController extends AbstractController
      * @var ArticleRepository
      */
     private $articles;
+    /**
+     * @var Session
+     */
+    private $session;
 
-    public function __construct(ArticleRepository $articles)
+    public function __construct(ArticleRepository $articles, SessionInterface $session)
     {
         $this->articles = $articles;
+        $this->session = $session;
     }
 
 
@@ -26,10 +33,15 @@ class PostListController extends AbstractController
       */
     public function listPosts()
     {
+        $this->session->start();
+        $views = $this->session->get('views') ?? 0 + 1;
+        $this->session->set('views', $views);
+
         $articles = $this->articles->findBy([], ['createdAt' => 'DESC']);
 
         return $this->render('posts.html.twig', [
             'title' => 'P2: Ukázkový dynamická aplikace',
+            'views' => $views,
             'articles' => $articles,
         ]);
     }
