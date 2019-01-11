a9cf2ae3d9ffd0d89c645dd080c67b2da5becb1a
Mikuláš Dítě <dite@mangoweb.cz>
2019-01-11 17:20:59 +0100

    todo

--- src/projects/p1-static/deploy.sh
+++ src/projects/p1-static/deploy.sh
@@ -3,4 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# TODO udelat to podle toho jako je to v praci:
+# - nejdriv nahrat assety
+# - az potom nahrat html
+
 rsync -avzh "$DIR/_site/" www-data@webserver-plain:/srv/p1/
