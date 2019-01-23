bcfad40a53d8653d9af2739bfe9cd13dca03a042
Mikuláš Dítě <dite@mangoweb.cz>
2019-01-23 21:11:42 +0100

    conditional progress figures

--- progress.sh
+++ progress.sh
@@ -3,17 +3,21 @@ set -euo pipefail
 IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-./preprocess.sh
+function count-lines() {
+    gsed -e '/MARK-START/,/MARK-END/{//!d}' main.txt | wc -l | awk '{print $1}'
+}
+
+./preprocess.sh -D'NOFIGURE=1'
 echo "Building version without TODOs"
 echo "" | xelatex -shell-escape main >/dev/null
 pdftotext main.pdf
-LINES_DONE="$(wc -l main.txt | awk '{print $1}')"
+LINES_DONE="$(count-lines)"
 
-./preprocess.sh -D'TODO=1'
+./preprocess.sh -D'TODO=1' -D'NOFIGURE=1'
 echo "Building version with TODOs"
 echo "" | xelatex -shell-escape main >/dev/null
 pdftotext main.pdf
-LINES_TODO="$(wc -l main.txt | awk '{print $1}')"
+LINES_TODO="$(count-lines)"
 
 PERCENT="$(bc <<< "scale=2; $LINES_DONE/$LINES_TODO*100")"
 echo "$LINES_DONE / $LINES_TODO ($PERCENT %)"
