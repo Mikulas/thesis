34dcf58259e19d321bf4d65b59adf9162cc3dd96
Mikuláš Dítě <dite@mangoweb.cz>
2019-01-26 14:40:33 +0100

    jenkins p1

--- src/ci-cd/jenkins/cookbooks/jenkins/recipes/default.rb
+++ src/ci-cd/jenkins/cookbooks/jenkins/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-package %w(curl ca-certificates openjdk-8-jdk) do
+package %w(curl ca-certificates openjdk-8-jdk rsync) do
   action :upgrade
 end
 
@@ -16,4 +16,14 @@ end
 package 'jenkins' do
   version '2.150.2'
   action :upgrade
+end
+
+package %w(docker.io) do
+  action :upgrade
+end
+
+group 'docker' do
+  action :modify
+  members 'jenkins'
+  append true
 end
